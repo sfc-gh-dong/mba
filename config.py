6c1335548d36bf90efd61b89edca5a2589c47a84
Daniel Ong <111833269+snowdann@users.noreply.github.com>
2022-10-27 23:30:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+snowflake_conn_prop = {
+   "account": "",
+   "user": "",
+   "password": "",
+   "role": "ACCOUNTADMIN",
+   "database": "FMCG_RETAIL_ID",
+   "schema": "PUBLIC",
+   "warehouse": "BI_DEMO_WH",
+}
+
+# Snowflake Account Identifiers
+# https://docs.snowflake.com/en/user-guide/admin-account-identifier.html#account-identifier-formats-by-cloud-platform-and-region
+#  
